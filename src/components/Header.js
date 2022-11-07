8db0bf95d93eba6cc1ef230094a2a35d21ac1ee9
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-11-07 11:38:41 +0600

    Fix Some Issued

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../firebase/UserContext";
-import { FaToggleOff } from "react-icons/fa";
 
 const Header = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -22,40 +21,47 @@ const Header = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="">
               <button className="fs-5 bold rounded me-3 border">
-                <Link to="/blog">Blog</Link>
+                <Link className="text-decoration-none" to="/blog">
+                  Blog
+                </Link>
               </button>
 
               <button className="fs-5 bold rounded me-3 border">
-                <Link to="/faq">FaQ</Link>
+                <Link className="text-decoration-none" to="/faq">
+                  FaQ
+                </Link>
               </button>
 
               <button className="fs-5 bold rounded me-3 border">
-                <Link to="/program">Courses</Link>
+                <Link className="text-decoration-none" to="/program">
+                  Courses
+                </Link>
               </button>
             </Nav>
             <Nav>
               {user?.uid ? (
                 <button onClick={logOut} className="btn btn-warning">
                   {" "}
-                  <Link to="/">SignOut</Link>{" "}
+                  <Link className="text-decoration-none" to="/">
+                    SignOut
+                  </Link>{" "}
                 </button>
               ) : (
                 <div>
                   <button className="btn btn-warning me-1">
-                    <Link to="/signup">Sign Up</Link>
+                    <Link className="text-decoration-none" to="/signup">
+                      Sign Up
+                    </Link>
                   </button>
                   <button className="btn btn-warning">
-                    <Link to="/login">Log In</Link>
+                    <Link className="text-decoration-none" to="/login">
+                      Log In
+                    </Link>
                   </button>
                 </div>
               )}
-
-              {/* Dark / Light */}
-              {/* <Nav.Link eventKey={2} href="">
-                <FaToggleOff className="fs-2"></FaToggleOff>
-              </Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
